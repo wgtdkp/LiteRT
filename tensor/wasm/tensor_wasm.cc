c447bcf30204eda17c636b59e7a33ea5aa96c94e
Quentin Khan <qkhan@google.com>
2026-05-15 05:40:36 -0700

    Remove RTTI from the Tensor API. Rework the `Buffer` hierarchy.
    This change removes virtual inheritance from the `Buffer` hierarchy.
    `MutableBuffer` is removed and `LockMutable()` is merged into `Buffer`.
    
    Instances of `dynamic_cast` on buffers should now use `Buffer::As<T>()`. This
    is also safer as it forces users to check that the cast was performed
    successfully.
    
    `GetTypeId()` and `IsA()` have been implemented for the existing buffer types
    and let us retrieve the concrete type of a buffer when it's kept as part of the
    graph and are used by `Buffer::As<T>()` to safely downcast from a buffer.
    
    LiteRT-PiperOrigin-RevId: 915955881

--- tensor/wasm/tensor_wasm.cc
+++ tensor/wasm/tensor_wasm.cc
@@ -171,12 +171,11 @@ val GetMutableTensorData(const TensorHandle& tensor) {
   }
 
   auto& buffer = *statusor_buf;
-  auto* mutable_buffer = dynamic_cast<litert::tensor::MutableBuffer*>(&buffer);
-  if (!mutable_buffer) {
+  auto span = buffer.LockMutable();
+  if (span.data() == nullptr) {
     return val::null();
   }
 
-  auto span = mutable_buffer->LockMutable();
   const size_t bytes = span.size();
   void* ptr = span.data();
 
@@ -210,11 +209,10 @@ emscripten::val GetTensorWebGpuBufferId(const TensorHandle& tensor) {
   auto statusor_buf = tensor.GetBuffer();
   if (!statusor_buf.ok()) return emscripten::val::null();
 
-  auto* litert_buffer =
-      dynamic_cast<litert::tensor::LitertBuffer*>(&*statusor_buf);
-  if (!litert_buffer) return emscripten::val::null();
+  auto litert_buffer_or = statusor_buf->As<litert::tensor::LitertBuffer>();
+  if (!litert_buffer_or.ok()) return emscripten::val::null();
 
-  auto& tb = litert_buffer->tensor_buffer();
+  auto& tb = litert_buffer_or->tensor_buffer();
   LiteRtTensorBuffer raw_tb = tb.Get();
   auto* tb_t = reinterpret_cast<LiteRtTensorBufferT*>(raw_tb);
 
@@ -886,14 +884,12 @@ EMSCRIPTEN_BINDINGS(litert_tensor_core) {
               return 0;
             }
             auto& buffer = *statusor_buf;
-            auto* mutable_buffer =
-                dynamic_cast<litert::tensor::MutableBuffer*>(&buffer);
-            if (!mutable_buffer) {
-              ABSL_LOG(ERROR) << "JITSI_LOG: getDataPointer: dynamic_cast to "
-                                 "MutableBuffer failed!";
+            auto span = buffer.LockMutable();
+            if (span.data() == nullptr) {
+              ABSL_LOG(ERROR)
+                  << "JITSI_LOG: getDataPointer: LockMutable failed!";
               return 0;
             }
-            auto span = mutable_buffer->LockMutable();
             return reinterpret_cast<uintptr_t>(span.data());
           }) LITERT_EM_ASYNC)
       .function(
